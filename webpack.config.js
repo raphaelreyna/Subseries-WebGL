a271e14792f0cba46b13fe6acc97f8183ff1f0de
Raphael Reyna <raphaelreyna@protonmail.com>
2019-07-27 00:13:14 -0700

    Use @rreyna/react-tracker-canvas

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/main.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+};
