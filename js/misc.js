b6e7305a6f210856ca7a5ecbb0843e808b0f523a
Raphael Reyna <raphaelreyna@protonmail.com>
2022-10-03 12:15:52 -0700

    update packahe lock and graph url

--- js/misc.js
+++ js/misc.js
@@ -1,5 +1,5 @@
 const BASE = 255;
-const graphURL = `https://www.rphlrn.com/sympy-graph`;
+const graphURL = `https://api.rphlrn.com/sympy-graph`;
 const expandQuery = `
 query expand($function: String!, $degree: Int!) {
   taylor(function: $function, degree: $degree) {
